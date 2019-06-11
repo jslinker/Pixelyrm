46ff9335dfaeb26f1a6b3649c8da4ceed9562740
Michael Daniels <mikeytdan@gmail.com>
2019-06-10 20:10:03 -0600

    Initial Commit

---
+++ Pixelyrm/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pixelyrm
+//
+//  Created by Michael Daniels on 6/10/19.
+//  Copyright © 2019 Michael Daniels. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
